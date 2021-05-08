de0e90e067a3232ff01c94aa7c5a26dd540dd0b1
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-08 21:03:35 +0900

    サーバ側の Graceful Shutdown を実装

--- server/server.js
+++ server/server.js
@@ -30,18 +30,24 @@ intervalObj = setInterval(() => {
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  body = JSON.stringify(response)
+  const body = JSON.stringify(response)
 
   res.end(body);
-  console.log(body);
+  const now = Date.now()
+  console.log(now + ' | body: ' + body);
 }).listen(PORT, () => console.log('Server http://localhost:' + PORT));
 
 process.on('SIGTERM', () =>{
-  console.log('Received SIGTERM at ' + (new Date()));
-  server.close(() => {
-    console.log('Server closed');
-  });
+  // リクエストの処理中かもしれないので 5 秒待つ
+  setTimeout(() => {
 
-  clearInterval(intervalObj);
-  console.log('intervalObj cleared');
+    console.log('Received SIGTERM at ' + (new Date()));
+    server.close(() => {
+      console.log('Server closed');
+    });
+
+    clearInterval(intervalObj);
+    console.log('intervalObj cleared');
+
+  }, 5000)
 });
