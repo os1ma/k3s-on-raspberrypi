e45449e824e507f5a1fbf2529ca2e1ae2fc2e9a7
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-08 22:57:26 +0900

    フォーマッタを適用

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const http = require('http');
-const process = require('process');
+const http = require("http");
+const process = require("process");
 
-const PORT = 3000
+const PORT = 3000;
 const INTERVAL_MILLIS = process.env.INTERVAL_MILLIS || 1000;
 
 const ON_RESPONSE = {
-  "gpios": [
+  gpios: [
     {
-      "pin": 21,
-      "value": 1
-    }
-  ]
+      pin: 21,
+      value: 1,
+    },
+  ],
 };
 
 const OFF_RESPONSE = {
-  "gpios": [
+  gpios: [
     {
-      "pin": 21,
-      "value": 0
-    }
-  ]
+      pin: 21,
+      value: 0,
+    },
+  ],
 };
 
 var response = ON_RESPONSE;
@@ -28,26 +28,26 @@ intervalObj = setInterval(() => {
   response = response === ON_RESPONSE ? OFF_RESPONSE : ON_RESPONSE;
 }, INTERVAL_MILLIS);
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  const body = JSON.stringify(response)
+const server = http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    const body = JSON.stringify(response);
 
-  res.end(body);
-  const now = Date.now()
-  console.log(now + ' | body: ' + body);
-}).listen(PORT, () => console.log('Server http://localhost:' + PORT));
+    res.end(body);
+    const now = Date.now();
+    console.log(now + " | body: " + body);
+  })
+  .listen(PORT, () => console.log("Server http://localhost:" + PORT));
 
-process.on('SIGTERM', () =>{
+process.on("SIGTERM", () => {
   // リクエストの処理中かもしれないので 5 秒待つ
   setTimeout(() => {
-
-    console.log('Received SIGTERM at ' + (new Date()));
+    console.log("Received SIGTERM at " + new Date());
     server.close(() => {
-      console.log('Server closed');
+      console.log("Server closed");
     });
 
     clearInterval(intervalObj);
-    console.log('intervalObj cleared');
-
-  }, 5000)
+    console.log("intervalObj cleared");
+  }, 5000);
 });
